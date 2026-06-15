5ccd31a39f658716479fd071da910370a0750627
ayaanronaldo08-hub <ayaanronaldo08@gmail.com>
2026-06-15 13:45:06 -0500

    Add files via upload

---
+++ L5A1.java
@@ -0,0 +1,14 @@
+public class L5A1 {
+    public static void main(String[] args) {
+        int num = 10;
+
+        while(num > 0){
+            num -= 1;
+            System.out.println(num);
+        }
+
+        for (int i = 10; i > 0; i--){
+            System.out.println(i);
+        }
+    }
+}
